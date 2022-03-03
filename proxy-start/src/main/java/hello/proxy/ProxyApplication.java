ba7a604c1d287df71536c24d41fd3f16d747ed6a
JOOHYUNZZANG\dlwngus <joohyun333@naver.com>
2022-03-04 01:47:50 +0900

    동적 jdk proxy 적용(interface)

--- proxy-start/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy-start/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,9 +1,7 @@
 package hello.proxy;
 
-import hello.proxy.config.AppV1Config;
-import hello.proxy.config.AppV2Config;
-import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
-import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -13,8 +11,10 @@ import org.springframework.context.annotation.Import;
 
 //@Import(AppV1Config.class)
 //@Import({AppV1Config.class,AppV2Config.class})
-//@Import(InterfaceProxyConfig.class)
-@Import(ConcreteProxyConfig.class)
+//@Import(InterfaceProxyConfig.class) // interface 에 직접프록시로 LogTrace 적용
+//@Import(ConcreteProxyConfig.class) // 구현체에 직접프록시로 LogTrace 적용
+//@Import(DynamicProxyBasicConfig.class) // 모든 매서드에 동적프록시로 LogTrace 적용
+@Import(DynamicProxyFilterConfig.class)// 패턴이 일치하는 모든 매서드에 동적프록시로 LogTrace 적용(no-log제외)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
--- proxy-start/src/main/java/hello/proxy/config/v2_dynamicproxy/handler/LogTraceFilterHandler.java
+++ proxy-start/src/main/java/hello/proxy/config/v2_dynamicproxy/handler/LogTraceFilterHandler.java
@@ -2,22 +2,36 @@ package hello.proxy.config.v2_dynamicproxy.handler;
 
 import hello.proxy.trace.TraceStatus;
 import hello.proxy.trace.logtrace.LogTrace;
+import org.springframework.util.PatternMatchUtils;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
-public class LogTraceBasicHandler implements InvocationHandler {
+/**
+ * no-log 의  로그를 남기지 않기 위한 Handler
+ */
+public class LogTraceFilterHandler implements InvocationHandler {
 
     private final Object target;
     private final LogTrace logTrace;
+    private final String[] patterns; // 메서드 명이 해당 pattern 일때만 로그를 남김.
 
-    public LogTraceBasicHandler(Object target, LogTrace logTrace) {
+    public LogTraceFilterHandler(Object target, LogTrace logTrace, String... pattern) {
         this.target = target;
         this.logTrace = logTrace;
+        this.patterns = pattern;
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+        // 메서드 이름 필터
+        String methodName = method.getName();
+        // save, request, reque*, *est -> 패턴 적용
+        if (!PatternMatchUtils.simpleMatch(patterns, methodName)){
+            return method.invoke(target, args);
+        }
+
 
         TraceStatus status = null;
 
