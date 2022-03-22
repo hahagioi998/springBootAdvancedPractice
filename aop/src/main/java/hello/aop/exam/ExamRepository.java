943070b5e8f39113aa90da0720f2203d6271015a
JOOHYUNZZANG\dlwngus <joohyun333@naver.com>
2022-03-23 02:47:16 +0900

    스프링 AOP - 포인트 컷

---
+++ aop/src/main/java/hello/aop/exam/ExamRepository.java
@@ -0,0 +1,13 @@
+package hello.aop.exam;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ExamRepository {
+
+    private static int seq = 0;
+
+    /**
+     * 5번에 1번 실패하는 요청
+     */
+}
