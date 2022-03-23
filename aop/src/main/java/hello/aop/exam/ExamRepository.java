7c6e4ff4ad55c3ca648c4407777a047cc1fdd901
JOOHYUNZZANG\dlwngus <joohyun333@naver.com>
2022-03-24 00:37:54 +0900

    스프링 AOP Example

--- aop/src/main/java/hello/aop/exam/ExamRepository.java
+++ aop/src/main/java/hello/aop/exam/ExamRepository.java
@@ -1,5 +1,7 @@
 package hello.aop.exam;
 
+import hello.aop.exam.annotation.Retry;
+import hello.aop.exam.annotation.Trace;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -8,6 +10,15 @@ public class ExamRepository {
     private static int seq = 0;
 
     /**
-     * 5번에 1번 실패하는 요청
+     * 3번에 1번 실패하는 요청 - 시스템에서 간헐적 에러가 뜨는 경우를 상정
      */
+    @Trace
+    @Retry(value = 4)
+    public String save(String itemId){
+        seq++;
+        if(seq % 3 == 0){
+            throw new IllegalStateException("예외 발생");
+        }
+        return "ok";
+    }
 }
